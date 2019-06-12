1c1887e0c1272c6214765382b1afe456dd89be1c
Thiago Novaes <tbnovaes.dev@gmail.com>
2019-06-12 09:37:50 -0400

    issue reproduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyLibModule } from '@lib/my-lib';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
